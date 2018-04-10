d26fbdeea4162bd537414675c532898e63c9f391
Kim Gonzales <Kimgo589@gmail.com>
2018-04-10 16:45:37 -0400

    create table migrations

---
+++ db/migrate/02_create_shelves.rb
@@ -0,0 +1,13 @@
+class CreateShelves < ActiveRecord::Migration[4.2]
+
+    def up 
+      create_table :shelves do |t|
+        t.string :name 
+        t.integer :user_id
+      end 
+    end 
+  
+    def down
+      drop table :shelves
+    end 
+  end 
