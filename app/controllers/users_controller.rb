72fa5ff5bc9f4e7d0e66240c11f6eb8d475111eb
Kim Gonzales <Kimgo589@gmail.com>
2018-04-10 18:48:54 -0400

    login and signup views rendering properly with routes. add links to index page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,13 @@ class UsersController < ApplicationController
   get '/users' do 
     erb :'users/users'
   end 
+
+  get '/login' do 
+    erb :'/users/login'
+  end 
+
+  get '/signup' do
+    erb :'users/signup' 
+  end 
   
 end 
