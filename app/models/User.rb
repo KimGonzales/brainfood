b4c6e0cc304028710c2c50ee67be9e500afc7d0c
Kim Gonzales <Kimgo589@gmail.com>
2018-04-10 22:20:18 -0400

    add #slug to user class

--- app/models/User.rb
+++ app/models/User.rb
@@ -1,4 +1,9 @@
 class User < ActiveRecord::Base 
   has_many :shelves
-  has_many :books, through: :shelves 
+  has_many :books, through: :shelves
+
+  def slug 
+    self.username.downcase.gsub(" ","-")
+  end 
+
 end 
