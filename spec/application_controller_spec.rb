2edbae45f7ca68bb8851d32878737e87650ba3a3
Kim Gonzales <Kimgo589@gmail.com>
2018-04-11 00:01:05 -0400

    add signup and logout specs to controller spec

--- spec/application_controller_spec.rb
+++ spec/application_controller_spec.rb
@@ -68,9 +68,79 @@ describe ApplicationController do
       session = {}
       session[:user_id] = user.id
       get '/signup'
-      expect(last_response.location).to include('/tweets')
+      expect(last_response.location).to include('/users')
     end
   end 
-  
+
+  describe "Login" do
+
+    it 'loads the login page' do 
+      get '/login'
+      expect(last_response.status).to eq(200)
+    end
+
+    it 'loads the users page after login' do 
+      user = User.create(:username => 'sasha', :email => 'sasha@sasha.com', :password => '123456')
+      params = {
+        :username => 'sasha',
+        :password => '123456'
+      }
+      post '/login', params 
+      expect(last_response.status).to eq(302)
+      follow_redirect!
+      expect(last_response.status).to eq(200)
+      expect(last_response.body).to include("Check out what other's are reading")
+    end
+    
+    it 'does not let user view login page if already logged in' do 
+      user = User.create(:username => 'sasha', :email => 'sasha@sasha.com', :password => '123456')
+
+      params = {
+        :username => 'sasha',
+        :password => '123456'
+      }
+      post '/login', params
+      session ={}
+      session[:user_id] = user.id 
+      get '/login'
+      expect(last_response.location).to include('/users')
+    end
+  end
+
+  describe "logout" do
+    it "lets a user logout if they are already logged in" do
+      user = User.create(:username => "sasha", :email => "sasha@sasha.com", :password => "123456")
+
+      params = {
+        :username => "sasha",
+        :password => "123456"
+      }
+      post '/login', params
+      get '/logout'
+      expect(last_response.location).to include("/login")
+    end
+
+    it 'does not let a user logout if not logged in' do
+      get '/logout'
+      expect(last_response.location).to include("/")
+    end
+
+    it 'does not load /users if user not logged in' do
+      get '/users'
+      expect(last_response.location).to include("/login")
+    end
+
+    it 'does load /users if user is logged in' do
+      user = User.create(:username => "sasha", :email => "sasha@sasha.com", :password => "123456")
+
+
+      visit '/login'
+
+      fill_in(:username, :with => "sasha")
+      fill_in(:password, :with => "123456")
+      click_button 'submit'
+      expect(page.current_path).to eq('/users')
+    end
+  end
 end 
         
