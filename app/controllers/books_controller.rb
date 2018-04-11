18419bbc115bc7e93f004de705fb25e7cb40a05d
Kim Gonzales <Kimgo589@gmail.com>
2018-04-11 17:53:28 -0400

    edit /books to use ternary operator

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,8 @@
 class BooksController < ApplicationController
 
   get '/books' do
-    if logged_in?
-      @user = current_user
-      erb :'books/books'
-    else 
-      redirect to '/login'
-    end
-  end 
+    (logged_in?) ? (erb :'books/books') : (redirect to '/login')
+  end
 
   #CRUD
 
---
+++ notes.md
@@ -0,0 +1,7 @@
+To Do:
+
+User Login
+- [ ] flash error message if user not found or incorrect data is inputed into form.
+- [ ] let users create, update, view or delete shelves
+- [ ] let users create, update view or delete books 
+
