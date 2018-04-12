db456c5c4fa7c46b5d402e4f2c8caef0d49052b9
Kim Gonzales <Kimgo589@gmail.com>
2018-04-11 23:20:09 -0400

    remove id and label for in login erb

--- app/controllers/shelves_controller.rb
+++ app/controllers/shelves_controller.rb
@@ -1,4 +1,15 @@
 class ShelvesController < ApplicationController
-  #CREATE, READ, UPDATE DELETE 
+  #CREATE, READ, UPDATE DELETE
+
+  get '/shelves/new' do
+    if logged_in? then erb :'shelves/new' else redirect to '/' end
+  end
+
+  post '/shelves' do
+    #shelf = Shelf.new(params[:shelf])
+    #current_user.shelves << shelf
+    binding.pry 
+
+  end 
 
 end
