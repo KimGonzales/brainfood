0c53bfa7c95b2ed4fc9d9999c296455743dad1c8
Kim Gonzales <Kimgo589@gmail.com>
2018-04-12 15:20:25 -0400

    remove slugifiable from shelf and book

--- app/models/Book.rb
+++ app/models/Book.rb
@@ -2,13 +2,5 @@ class Book < ActiveRecord::Base
 
   belongs_to :shelf
   has_one :user, through: :shelf
-
-  def slug
-    self.title.downcase.gsub(" ", "-")
-  end 
-
-  def self.find_by_slug(slug)
-    self.all.detect{|book| book.slug == slug}
-  end
   
 end
--- app/models/Shelf.rb
+++ app/models/Shelf.rb
@@ -1,12 +1,4 @@
 class Shelf < ActiveRecord::Base 
   belongs_to :user
   has_many :books
-
-  def slug 
-    self.name.downcase.gsub(" ","-")
-  end
-
-  def self.find_by_slug(slug)
-    self.all.find{|shelf| shelf.slug = slug}
-  end
 end 
