158104abb8bb7401228016d1984a354b311fee6b
Kim Gonzales <Kimgo589@gmail.com>
2018-04-18 16:31:26 -0400

    delete extra require /config/env in application controller and delete require sinatra/base in books controller

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,3 @@
-require 'sinatra/base'
 class BooksController < ApplicationController
 
   get '/books' do
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -32,7 +32,6 @@ class UsersController < ApplicationController
     end
   end
 
-
   get '/profile' do
     login_checkpoint
     @user = current_user
